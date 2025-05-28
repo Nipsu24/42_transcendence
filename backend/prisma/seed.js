16c3ffafac2d98ff660e3c366a02da8dac5a874b
Marius Meier <mmeier@student.hive.fi>
2025-05-28 15:13:10 +0300

    changes https port of nginx from 443 to 8443, only seeds database in case it's empty

--- backend/prisma/seed.js
+++ backend/prisma/seed.js
@@ -2,14 +2,20 @@ const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
 async function main() {
-    await prisma.player.createMany({
-		data: [
-            { name: 'Player One', password: '23213' },
-            { name: 'Player Two', password: 'password123' },
-            { name: 'Player Three', password: 'qwerty' },
-        ]
-    });
-	console.log('Users created successfully!');
+    const count = await prisma.player.count();
+	if (count === 0) {
+		await prisma.player.createMany({
+			data: [
+				{ name: 'Player One', password: '23213' },
+				{ name: 'Player Two', password: 'password123' },
+				{ name: 'Player Three', password: 'qwerty' },
+			]
+		});
+		console.log('Users created successfully!');
+	}
+	else {
+		console.log('Database already seeded, skipping.')
+	}
 }
 
 main()
