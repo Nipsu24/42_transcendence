e94d84b96d7613023076dbfc48393ff8732bbf0b
Marius Meier <mmeier@student.hive.fi>
2025-06-03 16:02:27 +0300

    further iteration of player object schema

--- backend/schemas/player.js
+++ backend/schemas/player.js
@@ -1,11 +1,40 @@
 const playerBodySchema = {
-  type: 'object',
-  required: ['name', 'password'],
-  properties: {
-    name: { type: 'string' },
-    password: { type: 'string' }
-  },
-  additionalProperties: false
+	type: 'object',
+	required: ['name', 'password'],
+	properties: {
+		name: { type: 'string' },
+		password: { type: 'string' },
+		e_mail: { type: 'string' },
+		stats: {
+			type: 'object',
+			properties: {
+				victories: { type: 'integer' },
+				defeats: { type: 'integer' },
+				matches: {
+					type: 'array',
+					items: {
+						type: 'object',
+						properties: {
+							date: { type: 'string' },
+							opponent: { type: 'string' },
+							victory: { type: 'boolean' },
+							result: {
+								type: 'array',
+								items: { type: 'integer' }
+							},
+						},
+						additionalProperties: false
+					}
+				}
+			},
+			additionalProperties: false
+		},
+		friends: {
+			type: 'array',
+			items: { type: 'integer' }
+		},
+	},
+	additionalProperties: false
 };
 
 module.exports = { playerBodySchema };
